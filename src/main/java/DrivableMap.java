f876399a1ec80890bc537284ec0fe721c5472efe
David9Nagy <divadadd@gmail.com>
2022-10-09 21:59:30 -0300

    Done

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -1,11 +1,4 @@
-/* Below is the DrivableMap class, which is a map that contains a
- * HashMap mapping Strings to Drivable objects.
- * (Think of Python dictionaries as a comparison -- the concept is
- * similar!)
- *
- * Implement the requested methods as stated in the TODOs. We've
- * created the constructor for you already.
- */
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,40 +7,48 @@ import java.util.List;
 class DrivableMap {
     HashMap<String, Drivable> drivable_map;
 
-    /**
-     * A generic constructor that initializes car_map
-     * as an empty HashMap.
-     */
+
     public DrivableMap() {
         drivable_map = new HashMap<>();
     }
 
-    /* TODO: Write a method named addDrivable that takes a String (the ID)
-     *       and a Drivable object. If the ID string does not appear as a key
-     *       in drivable_map, then add the pair to drivable_map.
-     *       Return true if the Drivable was added to drivable_map.
-     */
+
+    public boolean addDrivable(String id, Drivable item) {
+        if (!this.drivable_map.containsKey(id)) {
+            this.drivable_map.put(id, item);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 
 
-    /* TODO: Write a method named hasFasterThan that takes an int (a speed)
-     *       and returns true iff there is at least one item in drivable_map
-     *       that has a maxSpeed >= the speed given.
-     * You may want to use drivable_map.keys() or drivable_map.values() to
-     * iterate through drivable_map.
-     */
+    public boolean hasFasterThan(int speed) {
+        for (Drivable item : this.drivable_map.values()) {
+            if (item.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
 
 
-    /* TODO: Write a method named getTradable that takes no arguments and
-     *       returns a List containing all of the Tradable items in
-     *       drivable_map.
-     */
+    public List<Tradable> getTradable() {
+        ArrayList<Tradable> list = new ArrayList<>();
+        for (Drivable item : this.drivable_map.values()) {
+            if (item instanceof Tradable) {
+                list.add((Tradable) item);
+            }
+        }
+        return list;
+    }
 
 
 
-    
+
 }
